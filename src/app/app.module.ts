36752717904e9d2159c706c1ceb074c778c5e321
datecasp <ciudadano.fake@gmail.com>
2023-02-22 23:17:23 +0100

    added some methods to service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,17 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TableCustomComponent } from './table-custom/table-custom.component';
 import { OlMapComponent } from './ol-map/ol-map.component';
+import { OlMapService } from './services/ol-map.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TableCustomComponent,
-    OlMapComponent,
-  ],
+  declarations: [AppComponent, TableCustomComponent, OlMapComponent],
   imports: [
     BrowserAnimationsModule,
     AppRoutingModule,
     NgbModule,
-    MaterialModule
+    MaterialModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [OlMapService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/ol-map.service.ts
+++ src/app/services/ol-map.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Coordinate } from 'ol/coordinate';
+import { ELEMENT_DATA } from '../data/element-data';
+import { IElement } from '../interfaces/IElement';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OlMapService {
+  data = ELEMENT_DATA;
 
-  constructor() { }
+  constructor() {}
+
+  public getDataService(): IElement[] {
+    return this.data;
+  }
+
+  public getRandomShwedAaccService(): IElement{
+    let randomPointId = Math.floor(Math.random() * 17);
+    return this.data[randomPointId];
+  }
+
+  private getPointsListService(): Coordinate[] {
+    let result: Coordinate[] = [];
+    for (let i = 0; i < this.data.length; i++) {
+      result.push(this.data[i].point);
+    }
+    return result;
+  }
 }
